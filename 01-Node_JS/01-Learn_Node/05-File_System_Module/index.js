880819f2faf4a7b51c8ca454275436f24dc2f90a
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2025-12-29 05:46:17 +0600

    feat: enhance File System Module with async file operations and error handling

--- 01-Node_JS/01-Learn_Node/05-File_System_Module/index.js
+++ 01-Node_JS/01-Learn_Node/05-File_System_Module/index.js
@@ -1,4 +1,5 @@
 console.log("File System Module");
+const { error } = require("console");
 const fs = require("fs");
 const path = require("path");
 
@@ -17,3 +18,31 @@ console.log(fs.readFileSync(text1File, "utf8"));
 fs.appendFileSync(text1File, "\nThis is the new line of the text 1 file.");
 
 console.log(fs.readFileSync(text1File, "utf8"));
+
+//async way
+
+const docFolder = path.join(__dirname, "docs");
+
+if (!fs.existsSync("docs")) {
+  fs.mkdirSync(docFolder);
+}
+
+const firstDoc = path.join(docFolder, "firstDoc.txt");
+
+fs.writeFile(firstDoc, "This is the first doc file.", (err) => {
+  if (err) throw err;
+});
+
+fs.readFile(firstDoc, "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+fs.appendFile(firstDoc, "\nThis is the new line of fist doc file.", (err) => {
+  if (err) throw err;
+});
+
+fs.readFile(firstDoc, "utf8", (err, updatedData) => {
+  if (err) throw err;
+  console.log(updatedData);
+});
